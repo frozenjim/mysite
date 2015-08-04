4036997009d36d0ad17a39f32d2528c83cf6502b
frozenjim <james@whitehousenorth.com>
2015-08-04 11:04:47 -0400

    Tutorial 3 - Raising Errors

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 # from django.template import RequestContext, loader
 
 from polls.models import Question
@@ -19,7 +19,10 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("you're looking at question %s." % question_id)
+    # return HttpResponse("you're looking at question %s." % question_id)
+
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
